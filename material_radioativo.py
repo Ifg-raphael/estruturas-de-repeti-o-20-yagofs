eaf6868341594e91da57842b3fdd517859782914
Yagofs <yagofsarmento8@gmail.com>
2025-09-01 20:10:11 -0300

    Solução final

--- material_radioativo.py
+++ material_radioativo.py
@@ -1,1 +1,15 @@
-# Sua solução aqui
+"""CÓDIGO PARA CALCULAR O TEMPO DE MEIA-VIDA DE UM MATERIAL RADIOATIVO"""
+#Inserção de valores
+massa = float(input("Digite a massa inicial do material radioativo (em gramas): "))
+tempo = 0
+
+#Laço para calcular enquanto a massa for maior que 0,5 gramas
+while massa > 0.5:
+    massa /= 2
+    tempo += 50
+
+# Cálculo do tempo em horas, minutos e segundos
+horas = tempo // 3600
+minutos = (tempo % 3600) // 60
+segundos = tempo % 60
+print(f"{horas}h {minutos}min {segundos}s")
